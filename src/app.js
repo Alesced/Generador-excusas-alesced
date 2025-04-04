a86d19a389ba39d15eb1a0488c79ef4dc1f60712
Alesced <alesc2413@gmail.com>
2025-04-04 21:28:23 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 
 window.onload = function() {
-  //write your code here
 let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
 let action = ['ate', 'peed', 'crushed', 'broke'];
 let what = ['my homework', 'my phone', 'the car'];
@@ -10,16 +9,15 @@ let when = ['before the class', 'when I was sleeping', 'while I was exercising',
 function randomnumber(max) {
   return Math.floor(Math.random() * max); 
 }
-  //genracion aleatoria who:
+  //generacion aleatoria who:
 let RandomWhoIndex = randomnumber(who.length) 
-  //genracion aleatoria action:
+  //generacion aleatoria action:
 let RandomActionIndex = randomnumber(action.length) 
-  //genracion aleatoria what:
+  //generacion aleatoria what:
 let RandomWhatIndex = randomnumber(what.length) 
-  //genracion aleatoria when:
+  //generacion aleatoria when:
 let RandomWhenIndex = randomnumber(when.length) 
 
-let excuse = who[RandomWhoIndex] + " " + action[RandomActionIndex] + " " + what[RandomWhatIndex] + " " + when[RandomWhenIndex];
+document.querySelector("#excuse").innerHTML = who[RandomWhoIndex] + " " + action[RandomActionIndex] + " " + what[RandomWhatIndex] + " " + when[RandomWhenIndex];
 
-document.getElementById("excuse").innerHTML = excuse;
 };
