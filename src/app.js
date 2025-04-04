05b0c18a92e7871181158ed5f02884f3387b0d7f
Alesced <alesc2413@gmail.com>
2025-04-04 21:04:46 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+//generacion aleatoria
+function randomnumber(max) {
+  return Math.floor(Math.random() * max); 
+}
+  //genracion aleatoria who:
+let RandomWhoIndex = randomnumber(who.length) 
+  //genracion aleatoria action:
+let RandomActionIndex = randomnumber(action.length) 
+  //genracion aleatoria what:
+let RandomWhatIndex = randomnumber(what.length) 
+  //genracion aleatoria when:
+let RandomWhenIndex = randomnumber(when.length) 
+
+let excuse = who[RandomWhoIndex] + " " + action[RandomActionIndex] + " " + what[RandomWhatIndex] + " " + when[RandomWhenIndex];
+
+document.getElementById("excuse").innerHTML = excuse;
 };
